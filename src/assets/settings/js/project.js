c5925d9da90a64bb47d93780a88f315870951daf
allen_lin <power23313@gmail.com>
2023-10-02 04:50:11 +0800

    Add loading mask.

--- src/assets/settings/js/project.js
+++ src/assets/settings/js/project.js
@@ -1,4 +1,35 @@
+function ready() {
+    const totalTime = 3000; 
+    const incrementAmount = 1; 
+    const totalIncrements = 100; 
+    const incrementInterval = totalTime / totalIncrements;
+    let progressWidth = $('.progress').width() / 100;
+
+    let currentNumber = 0;
+    let timer;
+
+    function incrementNumber() {
+        if (currentNumber < 100) {
+            currentNumber += incrementAmount;
+            $('.progress-bar').text(currentNumber + '%');
+            
+            $('.progress-bar').width(progressWidth * currentNumber);
+            timer = setTimeout(incrementNumber, incrementInterval);
+        } else {
+            $('.loading-mask').addClass('loading-mask-over');
+            setTimeout(() => {
+                $('.loading-mask').hide();
+                $('body').css('overflow-y', 'auto');
+              }, 1000);
+        }
+    }
+
+    incrementNumber();
+}
+
 $(document).ready(function() {
+    ready();
+
     let topButton = $('#go_to_top');
 
     $(window).scroll(function()  {
